43a57cfb23fa88decb885039a6e31d13bf354418
AaronLiu615 <99209428+AaronLiu615@users.noreply.github.com>
2026-03-15 22:16:59 -0400

    secrets

--- scripts/fix-paths.js
+++ scripts/fix-paths.js
@@ -1,16 +1,16 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // Fix paths in index.html for GitHub Pages subdirectory deployment
-const indexPath = path.join(__dirname, '../dist/index.html');
+const indexPath = path.join(__dirname, "../dist/index.html");
 
-let html = fs.readFileSync(indexPath, 'utf-8');
+let html = fs.readFileSync(indexPath, "utf-8");
 
 // Replace absolute paths with relative paths
 html = html.replace(/href="\/favicon\.ico"/g, 'href="./favicon.ico"');
 html = html.replace(/src="\/_expo\//g, 'src="./_expo/');
 html = html.replace(/href="\/_expo\//g, 'href="./_expo/');
 
-fs.writeFileSync(indexPath, html, 'utf-8');
+fs.writeFileSync(indexPath, html, "utf-8");
 
-console.log('Fixed asset paths in dist/index.html');
+console.log("Fixed asset paths in dist/index.html");
